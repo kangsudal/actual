f6b617bc18febbeea1a98d9f95b5bfadbef83dbd
kangsudal_notebook <kangsudal@gmail.com>
2023-02-08 22:36:39 +0900

    레스토랑 상세페이지 작업하기

--- lib/restaurant/component/restaurant_card.dart
+++ lib/restaurant/component/restaurant_card.dart
@@ -10,6 +10,8 @@ class RestaurantCard extends StatelessWidget {
   final int deliveryTime; //배송걸리는 시간
   final int deliveryFee; //배송 비용
   final double ratings; //평균 평점
+  final bool isDetail; //상세 카드 여부
+  final String? detail; //상세 내용
 
   const RestaurantCard({
     Key? key,
@@ -20,9 +22,14 @@ class RestaurantCard extends StatelessWidget {
     required this.deliveryTime,
     required this.deliveryFee,
     required this.ratings,
+    this.isDetail = false,
+    this.detail,
   }) : super(key: key);
 
-  factory RestaurantCard.fromModel({required RestaurantModel model}) {
+  factory RestaurantCard.fromModel({
+    required RestaurantModel model,
+    bool isDetail = false,
+  }) {
     return RestaurantCard(
       image: Image.network(
         model.thumbUrl,
@@ -34,6 +41,7 @@ class RestaurantCard extends StatelessWidget {
       deliveryTime: model.deliveryTime,
       deliveryFee: model.deliveryFee,
       ratings: model.ratings,
+      isDetail: isDetail,
     );
   }
 
@@ -41,58 +49,72 @@ class RestaurantCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        ClipRRect(
-          child: image,
-          borderRadius: BorderRadius.circular(12),
-        ),
+        if (isDetail) image,
+        if (!isDetail)
+          ClipRRect(
+            child: image,
+            borderRadius: BorderRadius.circular(12),
+          ),
         const SizedBox(height: 16),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              name,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w500,
+        Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: isDetail ? 16.0 : 0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                name,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Text(
-              tags.join(' · '),
-              style: TextStyle(
-                color: BODY_TEXT_COLOR,
-                fontSize: 14,
+              const SizedBox(
+                height: 8,
               ),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Row(
-              children: [
-                _IconText(
-                  icon: Icons.star,
-                  label: ratings.toString(),
+              Text(
+                tags.join(' · '),
+                style: TextStyle(
+                  color: BODY_TEXT_COLOR,
+                  fontSize: 14,
                 ),
-                renderDot(),
-                _IconText(
-                  icon: Icons.receipt,
-                  label: ratings.toString(),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Row(
+                children: [
+                  _IconText(
+                    icon: Icons.star,
+                    label: ratings.toString(),
+                  ),
+                  renderDot(),
+                  _IconText(
+                    icon: Icons.receipt,
+                    label: ratings.toString(),
+                  ),
+                  renderDot(),
+                  _IconText(
+                    icon: Icons.timelapse_outlined,
+                    label: '$deliveryTime 분',
+                  ),
+                  renderDot(),
+                  _IconText(
+                    icon: Icons.monetization_on,
+                    label: deliveryFee == 0 ? '무료' : deliveryFee.toString(),
+                  ),
+                ],
+              ),
+              if (detail != null && isDetail)
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 16.0,
+                  ),
+                  child: Text(detail!),
                 ),
-                renderDot(),
-                _IconText(
-                  icon: Icons.timelapse_outlined,
-                  label: '$deliveryTime 분',
-                ),
-                renderDot(),
-                _IconText(
-                  icon: Icons.monetization_on,
-                  label: deliveryFee == 0 ? '무료' : deliveryFee.toString(),
-                ),
-              ],
-            )
-          ],
+            ],
+          ),
         ),
       ],
     );
---
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -0,0 +1,29 @@
+import 'package:actual/common/layout/default_layout.dart';
+import 'package:actual/restaurant/component/restaurant_card.dart';
+import 'package:flutter/material.dart';
+
+class RestaurantDetailScreen extends StatelessWidget {
+  const RestaurantDetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      title: "테스트:불타는 떡볶이",
+      child: Column(
+        children: [
+          RestaurantCard(
+            image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
+            name: 'testtest',
+            tags: ['test', 'test', 'test'],
+            ratingsCount: 100,
+            deliveryTime: 30,
+            deliveryFee: 3500,
+            ratings: 4.76,
+            isDetail: true,
+            detail: '상세설명blalfdlfdkjkaadaf',
+          ),
+        ],
+      ),
+    );
+  }
+}
